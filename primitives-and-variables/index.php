a77edb455d0a29f0e1e7c758ddf204e755b1b33d
Nick Hernandez <xnickhernandez@gmail.com>
2021-05-15 22:34:10 +0000

    Declaring variables and playing with strings.

---
+++ primitives-and-variables/index.php
@@ -0,0 +1,16 @@
+<?php
+
+  $full_name = 'Nick Hernandez';
+  $is_cool = True;
+  $total_pets = 1;
+
+  echo "$full_name is cool and has $total_pets pets";
+
+  echo 'single quote
+  strings can span
+  multiple lines but will
+  output multiple lines';
+
+  echo "\ndouble quote strings also \nspecial characteer uses \$, \"inner quote\""
+
+?>
