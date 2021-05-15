214071a50c6682bbe60c380d48b3988d09a78a78
Nick Hernandez <xnickhernandez@gmail.com>
2021-05-15 23:55:19 +0000

    completed exercise writing-html

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+    echo "<h1>This Website</h1>";
+    echo "<hr>";
+    echo "<p>This site is wicked sick bro</p>";
+  ?>
+
+</body>
+</html>
