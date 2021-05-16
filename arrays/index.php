84b466b6d40c09799401e1d9da5943f5c9d5c541
Nick Hernandez <xnickhernandez@gmail.com>
2021-05-16 02:30:19 +0000

    Familiarizing self with php arrays and array functions.

---
+++ arrays/index.php
@@ -0,0 +1,12 @@
+<?php
+
+  $colors = array('red', 'white', 'blue');
+  echo "America is " . $colors[0] . ", " . $colors[1] . ", and " . $colors[2] . ".\n";
+
+  $jedi = array('Anakin', 'Obi-Wan', "Mace Windu", "Quigong Jin");
+  $length = count($jedi);
+  echo "length: $length\n";
+  $lastIndex = $length - 1;
+  echo "last jedi: " . $jedi[$lastIndex];
+
+?>
